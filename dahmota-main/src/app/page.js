82d0eab82a5ab3c0d63056f55a9f41aaf5921ca2
Diego <diego@wevolt.com.au>
2023-07-12 21:51:32 +0800

    👌 IMPROVE:

---
+++ dahmota-main/src/app/page.js
@@ -0,0 +1,49 @@
+"use client";
+
+import { Button, Flex, Image, Text, Box, Divider } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Flex padding="15px" flexDirection="column">
+      <Flex
+        justifyContent="space-between"
+        w="full"
+        padding="10px"
+        borderRadius="20px"
+      >
+        <Flex flexDir="row">
+          <Image src="/dahmotta.svg" alt="logo" width="50px" />
+          <Flex direction="column">
+            <Text ml="15px" fontWeight="bold" fontSize="25px" color="#FFF">
+              DahMotta Tecnologia
+            </Text>
+            <Text ml="15px" fontWeight="bold" fontSize="10px" color="#FFF">
+              Expertise. Commitment. Value.
+            </Text>
+          </Flex>
+        </Flex>
+
+        <Flex align="center">
+          <Button color="#FFF" variant="unstyled">
+            Sobre
+          </Button>
+          <Button color="#FFF" variant="unstyled" marginX="20px">
+            Galeria
+          </Button>
+          <Button
+            color="#FFF"
+            rightIcon={
+              <Image src="/icons/svg/HeadPhone.svg" alt="logo" width="12px" />
+            }
+            variant="unstyled"
+          >
+            Contato
+          </Button>
+        </Flex>
+      </Flex>
+      <Flex backgroundColor="#FFF" flex="0.5">
+        <Image src="/BackgroundImage.jpg" />
+      </Flex>
+    </Flex>
+  );
+}
