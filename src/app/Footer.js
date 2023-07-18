bc7037c9b60acc0ef10ff054c5b628de01cc2c89
henriqueh97 <henriquehermes97@gmail.com>
2023-07-18 20:09:52 +0800

    up

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -2,10 +2,15 @@ const { Flex, Text } = require("@chakra-ui/react")
 
 const Footer = () => {
     return (
-        <Flex justify="space-between" bgGradient="linear-gradient(125deg,#c90f55 30%,#6c3a8e)" padding="25px">
+        <Flex
+            flexDir={{ base: "column", md: "row" }}
+            justify="space-between"
+            align="center"
+            bgGradient="linear-gradient(125deg,#c90f55 30%,#6c3a8e)"
+            padding="25px">
             <Text>Copyright 2023 - DahMotta Tecnologia - Electronics Company</Text>
 
-            <Text>contato@dahmotta.com.br</Text>
+            <Text marginTop={{ base: "10px", md: 0 }}>contato@dahmotta.com.br</Text>
         </Flex>
     )
 }
